e0c3eb8b8a4485ea77923caac8959bfdc7a97785
Leandre <leandre.blanchard@epitech.eu>
2019-09-04 13:52:06 +0900

    [DEV] fix thread tests

--- tests/thread_tests.c
+++ tests/thread_tests.c
@@ -2,32 +2,28 @@
 
 #include "lthread.h"
 
-void *func(void *data)
+void func(const lthread_t *thread)
 {
-	int *i = data;
+	int *i = thread->userdata;
 
 	*i = 1;
-	return (data);
 }
 
-void *funcf(void *data)
+void funcf(const lthread_t *thread)
 {
-	int *i = data;
+	int *i = thread->userdata;
 
 	*i = 2;
-	return (data);
 }
 
 Test(create, lthread)
 {
 	lthread_t thread;
 	int val = 0;
-	void *ret;
 
 	cr_assert(lthread_create(&thread, &func, &val) == 0);
 	lthread_launch(&thread, 0);
-	lthread_wait(&thread, &ret, -1);
-	cr_assert(ret == &val);
+	lthread_wait(&thread, -1);
 	cr_assert(val == 1);
 	lthread_destroy(&thread);
 }
@@ -36,17 +32,14 @@ Test(set_function, lthread)
 {
 	lthread_t thread;
 	int val = 0;
-	void *ret;
 
 	cr_assert(lthread_create(&thread, &func, &val) == 0);
 	cr_assert(lthread_launch(&thread, 0) == 0);
-	lthread_wait(&thread, &ret, -1);
-	cr_assert(ret == &val);
+	lthread_wait(&thread, -1);
 	cr_assert(val == 1);
 	lthread_set_function(&thread, &funcf, &val);
 	cr_assert(lthread_launch(&thread, 0) == 0);
-	lthread_wait(&thread, &ret, -1);
-	cr_assert(ret == &val);
+	lthread_wait(&thread, -1);
 	cr_assert(val == 2);
 	lthread_destroy(&thread);
 }
